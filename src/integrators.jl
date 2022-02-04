711805fd36105d4c3e22eddbed75653ebac5b1ac
abbaa90 <abbaa90@fu-berlin.de>
2022-02-04 01:09:08 +0100

    !

--- src/integrators.jl
+++ src/integrators.jl
@@ -1,14 +1,12 @@
 export update_parts_LD!
 export update_parts_BD!
 
+
 function update_parts_LD!(r::CuVector{SVector{N,T}}, v::CuVector{SVector{N,T}}, f::CuVector{SVector{N,T}},
     noise::CuVector{SVector{N,T}},sdot::CuVector{T},c1s::CuVector{T},c2s::CuVector{T},
-    c3s::CuVector{T},αs::CuVector{T},a::T; nthreads=128) where {N,T}
+    c3s::CuVector{T},αs::CuVector{T},a²::T; nthreads=128) where {N,T}
     Npart = UInt32(length(r))
-
-
-
-    CUDA.@sync @cuda blocks=ceil(Int, Npart/nthreads) threads=nthreads Langevin!(r, v, f, noise, sdot,c1s, c2s, c3s, αs, a)
+    CUDA.@sync @cuda blocks=ceil(Int, Npart/nthreads) threads=nthreads Langevin!(r, v, f, noise, sdot,c1s, c2s, c3s, αs, a²)
     return nothing
 end
 
@@ -16,9 +14,8 @@ export Langevin!
 
 function Langevin!(r::CuDeviceVector{SVector{N,T}}, v::CuDeviceVector{SVector{N,T}}, f::CuDeviceVector{SVector{N,T}},
      noise::CuDeviceVector{SVector{N,T}},sdot::CuDeviceVector{T},c1s::CuDeviceVector{T}, c2s::CuDeviceVector{T}, c3s::CuDeviceVector{T},
-     αs::CuDeviceVector{T}, a::T) where {N,T}
+     αs::CuDeviceVector{T}, a²::T) where {N,T}
      Npart = length(r)
-
      tid = threadIdx().x
      gtid = (blockIdx().x - 1) * blockDim().x + tid  # global thread id
 
@@ -33,9 +30,9 @@ function Langevin!(r::CuDeviceVector{SVector{N,T}}, v::CuDeviceVector{SVector{N,
              c3  = c3s[gtid]
              α   = αs[gtid]
              sdt = sdot[gtid]
-             pos = pos .+ c2 .* vel
-             vel = c1 .* vel .+ c3 .* rnd .+ c2 .* frc
-             sdt = (a*dot(vel,vel)- dot(c3 .* rnd,vel))/α
+             pos = pos .+ (c2/a²) .* vel
+             vel = c1 .* vel .+ c2 .* frc .+ c3 .* rnd
+             sdt = (dot(vel,vel)- dot(sqrt(2.0f0*α) .* rnd,vel))/(4.5530f0*α)
          end
          sync_threads()
          if gtid <= Npart
@@ -53,34 +50,31 @@ function update_parts_BD!(r::CuVector{SVector{N,T}}, v::CuVector{SVector{N,T}},
      f::CuVector{SVector{N,T}},noises::CuVector{SVector{N,T}}, sdot::CuVector{T},c1s::CuVector{T},
      c2s::CuVector{T},αs::CuVector{T}; nthreads=128) where {N,T}
     Npart = UInt32(length(r))
-
     CUDA.@sync @cuda blocks=ceil(Int, Npart/nthreads) threads=nthreads Brownian!(r, v, f, noises, sdot, c1s, c2s, αs)
     return nothing
 end
-
 
 export Brownian!
 
 function Brownian!(r::CuDeviceVector{SVector{N,T}}, v::CuDeviceVector{SVector{N,T}}, f::CuDeviceVector{SVector{N,T}},
      noise::CuDeviceVector{SVector{N,T}},sdot::CuDeviceVector{T},c1s::CuDeviceVector{T}, c2s::CuDeviceVector{T},αs::CuDeviceVector{T}) where {N,T}
      Npart = length(r)
-
      tid = threadIdx().x
      gtid = (blockIdx().x - 1) * blockDim().x + tid  # global thread id
 
      @inbounds begin
          if gtid <= Npart
              pos_tmp = r[gtid]
-             vel = v[gtid]
+             vel_tmp = v[gtid]
              frc = f[gtid]
              rnd = noise[gtid]
              c1  = c1s[gtid]
              c2  = c2s[gtid]
              α   = αs[gtid]
              sdt = sdot[gtid]
-             pos = pos_tmp .+ c1 .* frc + c2 .* rnd
-             vel = (pos .- pos_tmp) ./ c1
-             sdt = (dot(vel,vel)- dot(c2 .* rnd,vel))/α
+             #pos = pos_tmp .+ c1 .* frc + c2 .* rnd
+             #vel = (pos .- pos_tmp) ./ c1
+             #sdt = (dot(vel,vel) - dot(vel_tmp,vel_tmp)) /c1
          end
          sync_threads()
          if gtid <= Npart
--- src/simulation.jl
+++ src/simulation.jl
@@ -17,33 +17,19 @@ end
 
 
 function run_simulation!(dim::Int, Npart::Int, freq::Int, r₀::CuVector{SVector{N,T}},
-	v₀::CuVector{SVector{N,T}},Epot₀::CuVector{T},Ekin₀::CuVector{T},S₀::CuVector{T}, c₁₀::CuVector{T}, c₂₀::CuVector{T},
-	c₃₀::CuVector{T}, α₀::CuVector{T}, a::T,cut_off::T, periodicity::SVector{N,T},
-	forces_fun::Function,update_parts_LD::Function, noise2D::Function) where {N,T}
-    r  = copy(r₀)
-    v  = copy(v₀)
-
-
-    c1 = copy(c₁₀)
-    c2 = copy(c₂₀)
-    c3 = copy(c₃₀)
-    α  = copy(α₀)
-
-	f_int = zero(similar(v₀))
-    f_noise = zero(similar(v₀))
-    sdot  = zero(similar(S₀))
-	sdot_ave = similar(sdot)
-
-
+	v₀::CuVector{SVector{N,T}},S₀::CuVector{T}, c₁₀::CuVector{T}, c₂₀::CuVector{T}, c₃₀::CuVector{T},
+	α₀::CuVector{T}, a²::T, cut_off::T, periodicity::SVector{N,T},forces_fun::Function,
+	update_parts_LD!::Function, noise2D::Function) where {N,T}
     for _ in 1:freq
 		f_int = zero(similar(v₀))
 	    f_noise = zero(similar(v₀))
-        f_int = forces_fun(r,periodicity,cut_off)
+        f_int = forces_fun(r₀,periodicity,cut_off)
         f_noise = noise2D(Npart)
-        r, v, sdot = update_parts_LD(r, v, f_int, f_noise, c1, c2, c3, α, a)
-        r  = PBC!(r,periodicity)
+        update_parts_LD!(r₀, v₀, f_int, f_noise, S₀, c₁₀, c₂₀, c₃₀, α₀, a²)
+
+		PBC!(r₀,periodicity)
     end
-    return r, v, f_int, sdot
+    return nothing
 end
 
 
@@ -51,20 +37,7 @@ end
 function run_simulation!(dim::Int, Npart::Int, freq::Int, r₀::CuVector{SVector{N,T}},
 	v₀::CuVector{SVector{N,T}},S₀::CuVector{T}, c₁₀::CuVector{T}, c₂₀::CuVector{T},
 	α₀::CuVector{T},cut_off::T, periodicity::SVector{N,T},forces_fun::Function,
-	update_parts_BD::Function, noise2D::Function) where {N,T}
-    #r  = copy(r₀)
-    #v  = copy(v₀)
-
-
-    #c1 = copy(c₁₀)
-    #c2 = copy(c₂₀)
-    #α  = copy(α₀)
-
-    #f_int	= similar(v₀)
-    #f_noise = similar(v₀)
-    #sdot	= similar(S₀)
-
-
+	update_parts_BD!::Function, noise2D::Function) where {N,T}
     for _ in 1:freq
 		sdot	= zero(similar(S₀))
 		f_int = zero(similar(v₀))
